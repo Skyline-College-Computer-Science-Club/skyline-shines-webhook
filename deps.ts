97d5f9e567b9f35319588bda325acb2337be0063
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-03-23 15:11:58 -0700

    setup modules

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
+export type { RESTPostAPIWebhookWithTokenJSONBody } from "https://deno.land/x/discord_api_types@0.37.65/v10.ts";
--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+async function main() {
+  console.log("hi Deno");
+}
+
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("hi");
+  await main();
 }
